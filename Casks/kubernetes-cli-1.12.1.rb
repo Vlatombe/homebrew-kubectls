e301f4ca2e20decf268ad20685dd43420c7023a8
Vincent Latombe <vincent@latombe.net>
2018-10-09 12:09:26 +0200

    Update checksums

--- Casks/kubernetes-cli-1.12.1.rb
+++ Casks/kubernetes-cli-1.12.1.rb
@@ -1,5 +1,6 @@
 cask 'kubernetes-cli-1.12.1' do
     version '1.12.1'
+    sha256 '406949fbf4c319a7b723ac98d3785266ee50a94b368dc386abad947052e58232'
     #sha512 'df0c4a9e28da98e19cdeaf7ffaa1fdefd0581937889048fc01954fdacec737d66911e8d6dad36d00867b92656837e090cc146013e0c0d710c1c00c3d35d95ab8'
     
     url "https://dl.k8s.io/v#{version}/kubernetes-client-darwin-amd64.tar.gz"
--- Casks/kubernetes-cli-1.9.11.rb
+++ Casks/kubernetes-cli-1.9.11.rb
@@ -1,5 +1,6 @@
 cask 'kubernetes-cli-1.9.11' do
     version '1.9.11'
+    sha256 '27a0738ae6b2730b1a912ecbcfd0d1b213ad74c6a05365d2fead6f4a38b0f605'
     #sha512 'a83ff68d1ead35885a5ac6f3f1457e53d67448210bdccd96bbede823e9c1d93de9344f54dd714bd5779d20ed306d42e174711fe4928ffc9881cd2975a39f387f'
     
     url "https://dl.k8s.io/v#{version}/kubernetes-client-darwin-amd64.tar.gz"
